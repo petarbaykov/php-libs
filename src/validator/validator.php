b01ba465017aab512d0f6d711c57044b927413e5
Petar Baykov <petbaik26@gmail.com>
2020-02-27 10:20:37 +0200

    implement validate method

--- src/validator/validator.php
+++ src/validator/validator.php
@@ -7,8 +7,35 @@ class Validator {
 
     private $validation_rules = ["required", "email", "min", "max", "string", "boolean", "date", "file", "array", "numeric"];
 
-    public function validate() {
+    public function validate($data, $rules, $messages = []) {
+        $valid = false;
         
+        foreach($rules as $key => $rule) {
+            $rules_arr = explode("|", $rule);
+            foreach($rules_arr as $single_rule) {
+                $rule_params = explode(":",$single_rule);
+                $rule_name = $rule_params[0];
+                $rule_condition = isset($rule_params[1]) ? $rule_params[1] : false;
+
+                if(in_array($rule_name, $this->validation_rules)) {
+                    if($rule_name === "required") {
+                        if($this->empty($data[$key])) {
+                            return false;
+                        }
+                    }
+                }
+            }
+        }
+
+        return $valid;
+    }
+
+    private function empty($value) {
+        if(empty($value) || $value === "" || $value == "" || $value === null) {
+            return true;
+        }
+
+        return false;
     }
     
 }
